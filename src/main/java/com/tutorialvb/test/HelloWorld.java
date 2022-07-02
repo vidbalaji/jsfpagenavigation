3f162a44f1b020222b5077fcc1f9ae8eed362b19
vidbalaji <vidhya.balaji12@gmail.com>
2022-07-02 15:18:08 +0530

    check

---
+++ src/main/java/com/tutorialvb/test/HelloWorld.java
@@ -0,0 +1,29 @@
+package com.tutorialvb.test;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ManagedProperty;
+import javax.faces.bean.RequestScoped;
+
+@ManagedBean(name = "helloWorld", eager = true)
+@RequestScoped
+public class HelloWorld {
+   @ManagedProperty(value = "#{message}")
+   private Message messageBean;
+   private String message;
+   
+   public HelloWorld() {
+      System.out.println("HelloWorld started!");   
+   }
+   
+   public String getMessage() {
+      
+      if(messageBean != null) {
+         message = messageBean.getMessage();
+      }       
+      return message;
+   }
+   
+   public void setMessageBean(Message message) {
+      this.messageBean = message;
+   }
+}
---
+++ src/main/java/com/tutorialvb/test/Message.java
@@ -0,0 +1,16 @@
+package com.tutorialvb.test;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+
+@ManagedBean(name = "message", eager = true)
+@RequestScoped
+public class Message {
+   private String message = "Hello World!";
+	
+   public String getMessage() {
+      return message;
+   }
+   public void setMessage(String message) {
+      this.message = message;
+   }
+}
