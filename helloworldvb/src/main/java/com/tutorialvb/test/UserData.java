ed3b7c398702ffa94ec31d466697fa17d7425f3c
vidbalaji <vidhya.balaji12@gmail.com>
2022-07-03 19:14:29 +0530

    custom component

--- helloworldvb/src/main/java/com/tutorialvb/test/UserData.java
+++ helloworldvb/src/main/java/com/tutorialvb/test/UserData.java
@@ -9,6 +9,7 @@ import javax.faces.bean.SessionScoped;
 public class UserData implements Serializable {
    private static final long serialVersionUID = 1L;
    private String name;
+   private String password;
    
    public String getName() {
       return name;
@@ -16,6 +17,18 @@ public class UserData implements Serializable {
    
    public void setName(String name) {
       this.name = name;
+   }
+   
+   public String getPassword() {
+      return password;
+   }
+   
+   public void setPassword(String password) {
+      this.password = password;
+   }	
+   
+   public String login() {
+      return "result";
    }
 
    public String getWelcomeMessage() {
